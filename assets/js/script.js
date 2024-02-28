ffd5ba7a1693a5c4a12680a9c14a8a9458071420
chrialge <chrialge@gmail.com>
2024-02-28 17:55:39 +0100

    add exercise finish

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,70 @@
 console.log('ciao')
-
+// variabili che mi racchiude gli elelmneti della dom
 const containerElement = document.getElementById("container");
 console.log(containerElement);
 const difficultElement = document.getElementById('difficult')
 
 
-const numberCells = 100
-
-
-document.querySelector('form').addEventListener('submit', function(e){
+// quando clicco su avvia in base l,opzione scelta si puo avere una griglia
+document.querySelector('.button').addEventListener('click', function(e){
     e.preventDefault();
-    const valueDifficult = difficultElement.value
-    console.log(valueDifficult);
-    let numberCells = valueDifficult
+    // variabile che racchiude il valore di options
+    let numberCells = difficultElement.value;
+    
+    // questo ogni volta che clicco su avvia libera il container
+    containerElement.innerHTML = "";
 
-    let numberCremental = []
-for (let i = 0; i < numberCells; i++) {
-    const numberIndex = i + 1
-    console.log(numberIndex)
-    const markup = `
-    <div class="box">
-        <h1>${numberIndex}</h1>
-    </div>
-    `
-    containerElement.insertAdjacentHTML('beforeend', markup)
-    numberCremental.push(numberIndex)
-}
-console.log(numberCremental)
-
-
-
-const clickBoxElement = document.getElementsByClassName('box')
-for (let i = 0; i < clickBoxElement.length; i++) {
-    const clickBox = clickBoxElement[i];
-    clickBox.addEventListener('click', function(e){
-        e.preventDefault();
-        clickBox.classList.toggle('bg-azzure')
-        console.log(i + 1)
+    // il ciclo crea dei box in base al valore di numbercells
+    for (let i = 0; i < numberCells; i++) {
         
-    })
-}
-})
+        // variabile che racchiude i numeri tra 1 a 100 che sono uguali alla progressione dei cubi
+        const numberIndex = i + 1;
+
+        // variabile di markup
+        const markup = `
+        <div class="box">
+            <h1>${numberIndex}</h1>
+        </div>
+        `;
+        //inserisce dentro container il valore di markup
+        containerElement.insertAdjacentHTML('beforeend', markup);
+        
+        
+    }
+    // costante che racoglie tutti i box del dom
+    const clickBoxElement = document.getElementsByClassName('box');
+
+    // in base alla difficolta la griglia cambia
+    if(numberCells == 81){
+        for (let i = 0; i < clickBoxElement.length; i++) {
+            const clickBox = clickBoxElement[i];
+            clickBox.classList.add('square-9')
+            
+
+        };
+    }else if(numberCells == 49){
+        for (let i = 0; i < clickBoxElement.length; i++) {
+            const clickBox = clickBoxElement[i];
+            clickBox.classList.add('square-7')
+            
+
+        };
+    }
+
+
+   //ciclo per dare un evento ad ogni box
+    for (let i = 0; i < clickBoxElement.length; i++) {
+        const clickBox = clickBoxElement[i];
+
+        // se vengono cliccati devo diventare azzurri e loggare il numero suo
+        clickBox.addEventListener('click', function(e){
+            e.preventDefault();
+            clickBox.classList.toggle('bg-azzure');
+            console.log(i + 1);
+            
+        });
+        
+    };
+
+
+});
